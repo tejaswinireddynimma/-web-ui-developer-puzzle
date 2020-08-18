72b049d27dff51fc3fd4dff3200ca8506aca9a44
Tejaswini <tejaswinireddy.nimma09@gmail.com>
2020-08-18 16:37:41 -0400

    implemented quick search

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'tmo-book-search',
@@ -34,6 +35,14 @@ export class BookSearchComponent implements OnInit {
   ngOnInit(): void {
     this.store.select(getAllBooks).subscribe(books => {
       this.books = books;
+    });
+
+    this.searchForm.get('term').valueChanges
+    .pipe(debounceTime(500), // this will create a delay of 500ms 
+      distinctUntilChanged() // this will elemiate duplicate value search
+    )
+    .subscribe(val => {
+      this.searchBooks();
     });
   }
 
