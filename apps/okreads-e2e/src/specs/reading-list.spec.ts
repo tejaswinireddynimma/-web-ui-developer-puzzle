3250d75e58fc553207f7a4880b552e6bf9d88e6d
Tejaswini <tejaswinireddy.nimma09@gmail.com>
2020-08-19 16:08:06 -0400

    added test cases

--- apps/okreads-e2e/src/specs/reading-list.spec.ts
+++ apps/okreads-e2e/src/specs/reading-list.spec.ts
@@ -1,4 +1,5 @@
-import { $, browser, ExpectedConditions } from 'protractor';
+import { $, browser, ExpectedConditions, $$ } from 'protractor';
+import { expect } from 'chai';
 
 describe('When: I use the reading list feature', () => {
   it('Then: I should see my reading list', async () => {
@@ -16,5 +17,35 @@ describe('When: I use the reading list feature', () => {
         'My Reading List'
       )
     );
+  });
+
+  it('Then: I should able to finish the book', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
+
+    const form = await $('form');
+    const input = await $('input[type="search"]');
+    await input.sendKeys('javascript');
+    await form.submit();
+
+    const items = await $$('[data-testing="book-item"]');
+    expect(items.length).to.be.greaterThan(1, 'At least one book');
+
+    const addButton = await $('[data-testing="addButton"');
+    /// addButton.click();
+
+    const readingListToggle = await $('[data-testing="toggle-reading-list"]');
+    await readingListToggle.click();
+
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(
+        $('[data-testing="reading-list-container"]'),
+        'My Reading List'
+      )
+
+    );
+
   });
 });
